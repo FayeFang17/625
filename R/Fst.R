8ab43d26f23c7b7e1c2cc2167de82e4f1767a8ff
Faye Fang <fangn@umich.edu>
2021-12-06 18:06:02 -0500

    test12.6-1

--- R/Fst.R
+++ R/Fst.R
@@ -1,16 +1,89 @@
-#'Square
+#'FST
 #'
-#'Gets the square of a number
+#'@aliases the Fixation index
 #'
-#'@param x input value
+#'@description  A measure of population differentiation due to genetic structure.
+#'In this function, Fst is estimated from genetic polymorphism data, such as
+#'single-nucleotide polymorphisms(SNP). Calculation implemented by Hudson method
+#' in Hudson(1992).
 #'
-#'@return the square of x
+#'@param pop1 A m \times n matrix containing SNP values 0,1,2 of each of the n
+#'alleles of m sequences from population 1.
+#'
+#'@param pop2 A m \times n matrix containing SNP values 0,1,2 of each of the n
+#'alleles of m sequences from population 2.
+#'
+#'@param method a character string indicating format of output. This must be
+#''average'(default) or 'list'.
+#'
+#'@return the average FST value between the two populations.
+#'
+#'@references
+#'Bhatia, G., Patterson, N., Sankararaman, S., and Price, A.L. (2013).
+#'Hudson, R.R., Slatkin, M., and Maddison, W.P. (1992). Estimation of levels of
+#' gene flow from DNA sequence data. Genetics 132, 583-589.
 #'
 #'@examples
-#'square(3)
+#
+#'val=FST(pop1,pop2)
+#'The average Fst value between population 1 and population 2 is
+#'print(val)
+#'.
+#'
+#'val=FST(pop1,pop2,method='average')
+#'list=FST(pop1,pop2,method='list')
+#'The average Fst value between population 1 and population 2 is
+#'print(val)
+#'.Fst value for each allele is
+#'print(list)
+#'.
 #'
 #'@export
 #'
-square = function(x) {
-  return (x^2)
+#'
+
+FST <-function(pop1,pop2,method="NUll") {
+  #pop1
+
+  #calculating number of each allele types
+  no.AA=colSums(pop1==0)
+  no.AB=colSums(pop1==1)
+  no.BB=colSums(pop1==2)
+
+  #total genotypes
+  n1=(no.AA+no.AB+no.BB)*2
+  #frequency of A
+  pop1.fA=(no.AA*2+no.AB)/n1
+
+
+
+  #pop2
+
+  #calculating number of each allele types
+  no.AA=colSums(pop2==0)
+  no.AB=colSums(pop2==1)
+  no.BB=colSums(pop2==2)
+
+  #total genotypes
+  n2=(no.AA+no.AB+no.BB)*2
+
+  #frequency of A
+  pop2.fA=(no.AA*2+no.AB)/n2
+
+
+
+  #Fst calculation
+  N = (pop1.fA - pop2.fA)^2 - pop1.fA*(1-pop1.fA)/(n1-1) - pop2.fA*(1-pop2.fA)/(n2-1)
+  D = pop1.fA*(1-pop2.fA) + pop2.fA*(1-pop1.fA)
+  Fst.list=N/D
+
+  #Fst list
+  if (method=="list"){
+    return(Fst.list)
+  }
+
+  #average Fst
+  average=mean(Fst.list)
+
+  return (average)
 }
---
+++ R/data.prep.R
@@ -0,0 +1,26 @@
+#'data.prep
+#'
+#'Preapre data for Fst calculation. Parse SNP dataset by population.
+#'
+#'@param M A m \times n matrix containing SNP values 0,1,2 of each of the n
+#'alleles of m sequences.
+#'@param label A integer vector of population group label for each individuals.
+#'Indexing of label is the same as row index of M.
+#'
+#'@return  List of SNP matrix, each includes all individuals from a population.
+#'
+#'@examples
+#'
+#'data.prep(M,label)
+#'
+#'@export
+#'
+#'
+
+data.prep<-function(M,label){
+  pops=unique(label)
+  inds=sapply(1:length(pops), function(i) which(label %in% pops[i]))
+  separate.M=sapply(1:length(pops), function(i) M[inds[[i]],])
+  separate.M<-setNames(separate.M,pops)
+  return(separate.M)
+}
